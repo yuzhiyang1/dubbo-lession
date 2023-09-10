9cda8a9f815de18f90e7ce8ca4376f76e4900bde
yzy <1585004297@qq.com>
2023-09-10 12:41:24 +0800

    Spring 测试使用protobuf序列化方式实现服务接口调用

---
+++ dubbo-02-provider/src/main/java/com/yzy/service/HelloServiceImpl.java
@@ -0,0 +1,28 @@
+package com.yzy.service;
+
+import com.yzy.HelloRequest;
+import com.yzy.HelloResponse;
+import com.yzy.HelloService;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * hello服务impl
+ * 测试使用protobuf协议服务的实现类
+ *
+ * @author yzy
+ * @date 2023/09/10
+ */
+public class HelloServiceImpl implements HelloService {
+    @Override
+    public HelloResponse sayHello(HelloRequest request) {
+        System.out.println("HelloServiceImpl.sayHello");
+        System.out.println("request.name = " + request.getName());
+        return HelloResponse.newBuilder().setResult("this is sayHello result").build();
+    }
+
+    @Override
+    public CompletableFuture<HelloResponse> sayHelloAsync(HelloRequest request) {
+        return CompletableFuture.completedFuture(sayHello(request));
+    }
+}
--- dubbo-03-consumer/src/main/java/com/yzy/ConsumerMain.java
+++ dubbo-03-consumer/src/main/java/com/yzy/ConsumerMain.java
@@ -13,11 +13,16 @@ public class ConsumerMain {
                 new ClassPathXmlApplicationContext("applicationContext-consumer.xml");
 
         // 测试调用
-        UserService userService = (UserService) applicationContext.getBean("userService");
+        //UserService userService = (UserService) applicationContext.getBean("userService");
 
-        boolean login = userService.login("小明", "123456");
-        System.out.println("登录结果" + login);
+        //boolean login = userService.login("小明", "123456");
+        //System.out.println("登录结果" + login);
 
+        // 测试使用protobuf序列化方式调用的service
+        HelloService helloService = applicationContext.getBean(HelloService.class);
+        HelloRequest helloRequest = HelloRequest.newBuilder().setName("测试").build();
+        HelloResponse helloResponse = helloService.sayHello(helloRequest);
+        System.out.println("helloResponse.getResult = " + helloResponse.getResult());
         System.in.read();
     }
 }
